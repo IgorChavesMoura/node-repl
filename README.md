63938197afcbe9212d00fcc47c7ea3b111b1ca28
Igor Moura <igor310597@gmail.com>
2021-09-21 18:34:07 -0300

    repl

---
+++ README.md
@@ -0,0 +1,9 @@
+# Node Read-Eval-Print-Loop (REPL)
+
+## Node.js debugging concepts using inspect 
+
+### How to run inspect mode in terminal 
+    node inspect index.mjs
+
+### How to run inspect mode in browser (websocket debug port)
+    node --inspect index.mjs
---
+++ index.mjs
@@ -0,0 +1,33 @@
+//curl "localhost:3000?salary=3000&discount=15"
+
+import http from 'http';
+
+function netSalary({ discount, salary }) {
+
+    const percent = discount/100;
+
+    const cost = salary * percent;
+
+    const result = salary - cost;
+
+    return result;
+
+}
+
+
+http.createServer((req,res) => {
+
+    const url = req.url.replace('/', '');
+
+    const params = new URLSearchParams(url);
+
+    const data = Object.fromEntries(params);
+
+    const result = netSalary(data);
+
+    debugger;
+
+    res.end(`Your net salary is ${result}`);
+
+
+}).listen(3000, () => console.log('App running at port 3000'));
